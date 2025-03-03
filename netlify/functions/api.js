7c16a42d58a0a373b4d630aaf9274a6300757090
Puremind <92027990+puremind5@users.noreply.github.com>
2025-03-03 15:09:17 +0300

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,16 +6,10 @@ app.use(express.json());
 
 const router = express.Router();
 
-// Игровая логика
-router.post('/game/play', (req, res) => {
-  res.json({ message: 'Game logic will be here!' });
-});
-
-// Другие маршруты API
 router.get('/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
 
-app.use('/.netlify/functions/api', router);
+app.use('/api', router);
 
 module.exports.handler = serverless(app);
